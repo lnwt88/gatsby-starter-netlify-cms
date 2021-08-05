73b7c91d3c581f7c244f3478f7626d5b068dccc6
lnwt88 <86914960+lnwt88@users.noreply.github.com>
2021-08-05 18:27:31 +0700

    test
    s

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,7 +12,7 @@ const TemplateWrapper = ({ children }) => {
     <div>
       <Helmet>
         <html lang="en" />
-        <title>SEO TH - {title}</title>
+        <title>{title}</title>
         <meta name="description" content={description} />
 
         <link
@@ -23,13 +23,13 @@ const TemplateWrapper = ({ children }) => {
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix('/')}img/favicon-32x32.png`}
+          href={`${withPrefix('/')}img/logo.png`}
           sizes="32x32"
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix('/')}img/favicon-16x16.png`}
+          href={`${withPrefix('/')}img/logo.png`}
           sizes="16x16"
         />
 
